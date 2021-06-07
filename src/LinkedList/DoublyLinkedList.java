f16265cf15e24abfd770bb83804ebf03bf888900
adi160 <aditya@mindfiresolutions.com>
2021-06-07 08:45:25 +0530

    Doubly Linked List

--- src/LinkedList/DoublyLinkedList.java
+++ src/LinkedList/DoublyLinkedList.java
@@ -1,5 +1,7 @@
 package LinkedList;
 
+import java.util.NoSuchElementException;
+
 /**
  * Author : Aditya
  * Created on : 12:31 PM , 27/05/21
@@ -37,7 +39,7 @@ public class DoublyLinkedList {
         return length;
     }
 
-    public void atTheBeginning(int val){
+    public void atTheBeginningOfDoublyLinkedList(int val){
         ListNode newNode = new ListNode(val);
         if(isEmpty()){
             tail = newNode;
@@ -68,12 +70,67 @@ public class DoublyLinkedList {
         System.out.println("null");
     }
 
+    public void atTheEndOfDoublyLinkedList(int val){
+        ListNode newNode = new ListNode(val);
+
+        if(isEmpty()){
+            head = newNode;
+        }
+        else{
+            tail.next = newNode;
+            newNode.previous = tail;
+        }
+        tail = newNode;
+        length++;
+    }
+
+    public ListNode deleteFirstNode(){
+        if(isEmpty()){
+            throw new NoSuchElementException();
+        }
+        ListNode temp = head;
+        if(head == tail){
+            tail = null;
+        }
+        else{
+            temp.next.previous = null;
+        }
+        head = head.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+
+    public ListNode deleteLastNode(){
+        if(isEmpty()){
+            throw new NoSuchElementException();
+        }
+        ListNode temp = tail;
+        if(head == tail){
+            head = null;
+        }
+        else{
+            temp.previous.next = null;
+        }
+        tail = tail.previous;
+        temp.previous = null;
+        length--;
+        return temp;
+    }
+
     public static void main(String[] args) {
+
         DoublyLinkedList obj = new DoublyLinkedList();
-        obj.atTheBeginning(4);
-        obj.atTheBeginning(3);
+        obj.atTheBeginningOfDoublyLinkedList(4);
+        obj.atTheBeginningOfDoublyLinkedList(3);
+        obj.atTheEndOfDoublyLinkedList(5);
+        obj.atTheEndOfDoublyLinkedList(6);
+        obj.atTheBeginningOfDoublyLinkedList(2);
         obj.displayForward();
-        obj.displayBackward();
+//        obj.displayBackward();
+//        System.out.println(obj.deleteFirstNode().data);
+//        System.out.println(obj.deleteLastNode().data);
+//        obj.displayForward();
     }
 
 
